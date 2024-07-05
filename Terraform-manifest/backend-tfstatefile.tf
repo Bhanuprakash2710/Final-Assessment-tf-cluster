bda63b4e6f4d6e8d635457edade6350de5a90287
Bhanuprakash2710 <116619621+Bhanuprakash2710@users.noreply.github.com>
2024-07-05 12:17:02 +0530

    Update backend-tfstatefile.tf

--- Terraform-manifest/backend-tfstatefile.tf
+++ Terraform-manifest/backend-tfstatefile.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name = "rg-storage-accounts-1"
     storage_account_name = "tfstorageaccountbhanu"
     container_name = "tfstatefiles"
-    key = "app-service.terraform.tfstate"
+    key = "final-assessment-cluster.terraform.tfstate"
     
   }
 }
