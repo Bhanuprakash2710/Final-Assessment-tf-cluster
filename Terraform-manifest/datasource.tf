a7b317a1fb6f775907888eeb27ccd4d72b3b7ccc
Bhanuprakash2710 <116619621+Bhanuprakash2710@users.noreply.github.com>
2024-07-05 12:32:32 +0530

    Update datasource.tf

--- Terraform-manifest/datasource.tf
+++ Terraform-manifest/datasource.tf
@@ -1,4 +1,4 @@
-data "azurerm_kubernetes_service_versions" "current" {
+data "azurerm_kubernetes_service_versions" "current23" {
   location = azurerm_resource_group.rg2.location
   include_preview = false  
 }
